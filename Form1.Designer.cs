a59478d137927870316614073b0efa1aefe5dc1c
Merve Genç <38539228+merveeg@users.noreply.github.com>
2019-05-03 21:02:30 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace adamasmacaoyun
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pnlHarfler = new System.Windows.Forms.Panel();
+            this.btnOyunaBasla = new System.Windows.Forms.Button();
+            this.lblYanlis = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(177, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(176, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Harf Tahmin";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(560, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(228, 208);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(140, 107);
+            this.textBox1.MaxLength = 22;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(250, 20);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // pnlHarfler
+            // 
+            this.pnlHarfler.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlHarfler.Location = new System.Drawing.Point(57, 32);
+            this.pnlHarfler.Name = "pnlHarfler";
+            this.pnlHarfler.Size = new System.Drawing.Size(391, 69);
+            this.pnlHarfler.TabIndex = 12;
+            // 
+            // btnOyunaBasla
+            // 
+            this.btnOyunaBasla.Location = new System.Drawing.Point(93, 320);
+            this.btnOyunaBasla.Name = "btnOyunaBasla";
+            this.btnOyunaBasla.Size = new System.Drawing.Size(75, 23);
+            this.btnOyunaBasla.TabIndex = 14;
+            this.btnOyunaBasla.Text = "Oyuna Başla";
+            this.btnOyunaBasla.UseVisualStyleBackColor = true;
+            this.btnOyunaBasla.Click += new System.EventHandler(this.btnOyunaBasla_Click);
+            // 
+            // lblYanlis
+            // 
+            this.lblYanlis.AutoSize = true;
+            this.lblYanlis.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblYanlis.Location = new System.Drawing.Point(28, 253);
+            this.lblYanlis.Name = "lblYanlis";
+            this.lblYanlis.Size = new System.Drawing.Size(153, 23);
+            this.lblYanlis.TabIndex = 15;
+            this.lblYanlis.Text = "Denenen Harfler";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(207, 320);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Oyunu Bitir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblYanlis);
+            this.Controls.Add(this.btnOyunaBasla);
+            this.Controls.Add(this.pnlHarfler);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel pnlHarfler;
+        private System.Windows.Forms.Button btnOyunaBasla;
+        private System.Windows.Forms.Label lblYanlis;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace adamasmacaoyun
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            oyunuBaslat();
+        }
+        private void oyunuBaslat()
+        {
+            int solTaraf = 4;
+            pnlHarfler.Controls.Clear();
+            Random rnd = new Random(DateTime.Now.Millisecond);
+            int indis = rnd.Next(0, Kelimeler.Length); //Dizinin kaçıncı elemanını yazıcam
+            SeciliKelime = Kelimeler[indis];
+            //foreach (var item in SeciliKelime.ToCharArray())
+            //{
+            //    Label lbl = new Label();
+            //    lbl.AutoSize = false;
+            //    lbl.Text = "_";
+            //    lbl.Top = 5;
+            //    lbl.Left = solTaraf;
+            //    lbl.Tag = item.ToString();
+            //    lbl.Width = 15;
+            //    pnlHarfler.Controls.Add(lbl);
+            //    solTaraf += 15;
+
+            //    lbl.Refresh();
+            //}
+            for (int i = 0; i < SeciliKelime.Length; i++)
+            {
+                TextBox txtbx = new TextBox();
+                pnlHarfler.Controls.Add(txtbx);
+                txtbx.Top = 30;
+                txtbx.Left = solTaraf;
+                txtbx.Width = 30;
+                txtbx.Text = "_";
+                txtbx.Tag = SeciliKelime[i];
+                solTaraf += 40;
+                txtbx.BackColor = Color.LightSalmon;
+
+                txtbx.Refresh();
+
+
+                /*Label lbl = new Label();
+                lbl.AutoSize = false;
+                lbl.Text = "_";
+                lbl.Top = 5;
+                lbl.Left = solTaraf;
+                lbl.Tag = SeciliKelime[i];
+                lbl.Width = 15;
+                pnlHarfler.Controls.Add(lbl);
+                solTaraf += 15;
+                lbl.Refresh();
+                */
+            }
+            pnlHarfler.Refresh();
+            //lblYanlis.Clear();
+        }
+        int kullaniciHak = 0;
+        string lblText = "";
+        string SeciliKelime = "";
+        public string[] Kelimeler = { "OYUN", "GÖZ", "İSTANBUL","KALEM"};
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //pictureBox1.ImageLocation = @"C:\Users\33.PNG"
+            //pictureBox1.Load(@"C:\Users\33.PNG");
+            button1.BackColor = Color.Gray;
+            button1.ForeColor = Color.White;
+            pnlHarfler.BackColor = Color.DarkGreen;
+            var girilenHarf = textBox1.Text.ToUpper();
+            if (string.IsNullOrEmpty(girilenHarf))
+            {
+                return;
+            }
+            //List<string> matris = new List<string>();
+            //foreach (var item in lblYanlis.Text)
+            //{
+            //    matris.Add(item.ToString());
+            //}
+
+            //lblYanlis.Text = textBox1.Text;
+
+            if (lblYanlis.Text.ToUpper().Contains(girilenHarf))
+            {
+                MessageBox.Show("Bu harfi daha önce girmiştiniz!");
+                return;
+            }
+            
+
+            //lstYanlis.Items.Add(girilenHarf);
+            bool bulundu = false;
+
+            foreach (var item in pnlHarfler.Controls)
+            {
+                if (item is TextBox)
+                {
+                    var txtbx = item as TextBox;
+                    if (txtbx.Tag != null && txtbx.Tag.ToString().Equals(girilenHarf))
+                    {
+                        txtbx.Text = girilenHarf;
+                        bulundu = true;
+                    }
+                }
+
+            }
+            lblText = lblText + textBox1.Text + " ";
+            lblYanlis.Text = lblText;
+
+            if (!bulundu)
+            {
+                kullaniciHak++;
+
+            }
+            textBox1.Clear();
+
+            bool isEnd = true; 
+
+            if (kullaniciHak > 0)
+            {
+                pictureBox1.Image = Image.FromFile(@"C:\Users\Pic\" + kullaniciHak.ToString() + kullaniciHak.ToString() + ".PNG");
+                if (kullaniciHak == 9)
+                {
+                    pnlHarfler.BackColor = Color.Red;
+                    MessageBox.Show("Malesef,oyunu kaybettiniz.");
+                    
+                }
+
+            }
+            foreach (Control item in pnlHarfler.Controls)
+            { 
+                if (item is TextBox)
+
+                {
+                    var txtbx = item as TextBox;
+
+                    if (item.Text == "_")
+                        isEnd = false;
+                        
+                }
+            }
+            if(isEnd)
+                MessageBox.Show("Tebrikler,Kazandınız!");
+                pnlHarfler.BackColor = Color.White;
+
+        }
+        private void btnOyunaBasla_Click(object sender, EventArgs e)
+        {
+            oyunuBaslat();
+
+        }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                e.Handled = true;
+                button1.PerformClick();
+            }
+            else if (e.KeyChar == (char)Keys.Back)
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                if (char.IsLetter(e.KeyChar))
+                {
+                    e.Handled = textBox1.Text.Trim().Length == 1;
+                }
+                else
+                {
+                    e.Handled = true;
+                    MessageBox.Show("harften farklı karakter girdiniz ,lutfen harf giriniz:");
+
+                }
+            }
+    
+        }
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Form1 frm1 = new Form1();
+            frm1.Show();
+            this.Close();
+        }
+
+        /* private void lblYanlis_Click(object sender, EventArgs e)
+         {
+
+         }*/
+    }
+}
+
+
+
+
+
+    // }
+    //}
+
+    // private void textBox1_TextChanged(object sender, EventArgs e)
+    //{
+
+    // }
+
+    //private void textBox1_Click(object sender, EventArgs e)
+    //{
+    //    textBox1.Text = " ";
+
+    //}
+
+
+    //private void textBox1_TextChanged(object sender, EventArgs e)
+    //{
+
+    // }
+
+    // private void label1_Click(object sender, EventArgs e)
+    //{
+
+    // }
+
+    //private void label7_Click(object sender, EventArgs e)
+    // {
+
+    //}
+
+
+/*private void button2_Click(object sender, EventArgs e)
+{
+    string deneme = "ENGİN";
+    foreach (var item in deneme.ToCharArray())
+    {
+        MessageBox.Show(item.ToString());
+    }
+}
+
+private void textBox1_TextChanged(object sender, EventArgs e)
+{
+
+}*/
+
+
